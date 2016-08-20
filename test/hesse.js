c96b6628699d438785b9e6af1e3fb385c9cf2f39
Alex J Burke <alex@alexjeffburke.com>
2016-08-20 20:00:32 +0100

    Remove unnecessary outer promises from tests.

--- test/hesse.js
+++ test/hesse.js
@@ -20,72 +20,65 @@ describe('hesse', function () {
     it('should successfully run', function () {
         var port = 50000;
 
-        return expect(
-            expect.promise(function () {
-                var testProcess = childProcess.execFile('node', [basePath + 'bin/hesse.js', '--port', port, '--', 'node', 'testdata/example.js']);
+        return expect.promise(function () {
+            var testProcess = childProcess.execFile('node', [basePath + 'bin/hesse.js', '--port', port, '--', 'node', 'testdata/example.js']);
 
-                var httpPromise = expect.promise(function (resolve, reject) {
-                    testProcess.stderr.once('data', function (d) {
-                        expect('http://127.0.0.1:' + port, 'to have a running server').then(resolve).caught(reject);
-                    });
+            var httpPromise = expect.promise(function (resolve, reject) {
+                testProcess.stderr.once('data', function (d) {
+
+                    expect('http://127.0.0.1:' + port, 'to have a running server').then(resolve).caught(reject);
                 });
+            });
 
-                var exitPromise = expect.promise(function (run) {
-                    testProcess.on('exit', run(function (code) {
-                        return expect(code, 'to equal', 0);
-                    }));
-                });
+            var exitPromise = expect.promise(function (run) {
+                testProcess.on('exit', run(function (code) {
+                    return expect(code, 'to equal', 0);
+                }));
+            });
 
-                return expect.promise.all([httpPromise, exitPromise]);
-            }),
-            'to be fulfilled'
-        );
+            return expect.promise.all([httpPromise, exitPromise]);
+        });
     });
 
     it('should print stdout output', function () {
         var port = 50000;
 
-        return expect(
-            expect.promise(function () {
-                var testProcess = childProcess.execFile('node', [basePath + 'bin/hesse.js', '--port', port, '--', 'node', __dirname + '/../testdata/example.js']);
-                var outputBuffers = [];
+        return expect.promise(function () {
+            var testProcess = childProcess.execFile('node', [basePath + 'bin/hesse.js', '--port', port, '--', 'node', __dirname + '/../testdata/example.js']);
+            var outputBuffers = [];
 
-                testProcess.stdout.on('data', function (d) {
-                    outputBuffers.push(d);
-                });
+            testProcess.stdout.on('data', function (d) {
+                outputBuffers.push(d);
+            });
 
-                return expect.promise(function (run) {
-                    testProcess.on('exit', run(function (code) {
-                        setImmediate(function () {
-                            var stdoutLines = outputBuffers.map(function (b) {
-                                return b.toString();
-                            });
+            return expect.promise(function (run) {
+                testProcess.on('exit', run(function (code) {
+                    setImmediate(function () {
 
-                            expect(stdoutLines, 'to equal', [
-                                'RAN!!!'
-                            ]);
+                        var stdoutLines = outputBuffers.map(function (b) {
+                            return b.toString();
                         });
-                    }));
-                });
-            }),
-            'to be fulfilled'
-        );
+
+                        expect(stdoutLines, 'to equal', [
+                            'RAN!!!'
+                        ]);
+                    });
+                }));
+            });
+        });
     });
 
     it('should exit with the exit code of the test binary', function () {
         var port = 50000;
 
-        return expect(
-            expect.promise(function () {
-                var testProcess = childProcess.execFile('node', [basePath + 'bin/hesse.js', '--port', port, '--', 'node', __dirname + '/../testdata/exit2.js']);
+        return expect.promise(function () {
+            var testProcess = childProcess.execFile('node', [basePath + 'bin/hesse.js', '--port', port, '--', 'node', __dirname + '/../testdata/exit2.js']);
 
-                return expect.promise(function (run) {
-                    testProcess.on('exit', run(function (code) {
-                        return expect(code, 'to equal', 2);
-                    }));
-                });
-            }),
-            'to be fulfilled'
-        );
+            return expect.promise(function (run) {
+                testProcess.on('exit', run(function (code) {
+                    return expect(code, 'to equal', 2);
+                }));
+            });
+        });
     });
 });
